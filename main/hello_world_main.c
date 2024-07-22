03dbe019ca37c34c1da956b2c5467083fe129b29
evatikiotis <vatik.vag@gmail.com>
2024-07-22 22:33:26 +0200

    mdns config

--- main/hello_world_main.c
+++ main/hello_world_main.c
@@ -2,18 +2,31 @@
 #include "strip_handler.h"
 #include "wifi_connection_handler.h"
 #include "nvs_flash.h" //non volatile storage
-
-
-
+#include "mdns.h" 
 
 static const char *TAG = "main";
 
+void start_mdns_service()
+{
+    //initialize mDNS service
+    esp_err_t err = mdns_init();
+    if (err) {
+        printf("MDNS Init failed: %d\n", err);
+        return;
+    }
+
+    //set hostname
+    mdns_hostname_set("esp-hero");
+    //set default instance
+    mdns_instance_name_set("ESP smart light controller");
+}
 
 void app_main(void)
 {
 
     nvs_flash_init();
     wifi_connect();
+    start_mdns_service();
     strip_setup();
     bool led_on_off = true;
 
