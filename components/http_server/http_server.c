18378796632484c30c65482283795ec192a379ae
evatikiotis <vatik.vag@gmail.com>
2024-07-31 21:19:43 +0200

    registered two endpoints with different handlers

--- components/http_server/http_server.c
+++ components/http_server/http_server.c
@@ -2,9 +2,16 @@
 #include "http_server.h"
 
 
-static esp_err_t get_handler(httpd_req_t *req)
+static esp_err_t light_on_handler(httpd_req_t *req)
 {
-    char *response_message = "URI Response ...";
+    char *response_message = "Light ON!";
+    httpd_resp_send(req, response_message, HTTPD_RESP_USE_STRLEN);
+    return ESP_OK;
+}
+
+static esp_err_t light_off_handler(httpd_req_t *req)
+{
+    char *response_message = "Light OFF!";
     httpd_resp_send(req, response_message, HTTPD_RESP_USE_STRLEN);
     return ESP_OK;
 }
@@ -15,10 +22,18 @@ void start_http_server()
     httpd_handle_t server_handle = NULL;
     httpd_start(&server_handle, &server_config);
 
-    httpd_uri_t uri_get = {
-        .uri = "/",
+    httpd_uri_t uri_light_on = {
+        .uri = "/on",
         .method = HTTP_GET,
-        .handler = get_handler,
+        .handler = light_on_handler,
         .user_ctx = NULL};
-    httpd_register_uri_handler(server_handle, &uri_get);
+    httpd_register_uri_handler(server_handle, &uri_light_on);
+
+    httpd_uri_t uri_light_off = {
+        .uri = "/off",
+        .method = HTTP_GET,
+        .handler = light_off_handler,
+        .user_ctx = NULL};
+    httpd_register_uri_handler(server_handle, &uri_light_off);
+
 }
